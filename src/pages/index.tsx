b9fbc23b75f29a8c0ff8b014b1f4c2100505d65b
Szymon Sadowski <szymon.sadowski@boldare.com>
2023-02-05 18:48:51 +0100

    styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,20 @@
-import { api } from "../utils/api";
 import { type NextPage } from "next";
 import Head from "next/head";
+import Image from "next/image";
+import { useState } from "react";
+import { getRandomDrinks } from "src/utils/selectRandomDrinks";
 import { useDrink } from "src/utils/useDrink";
 
 const Home: NextPage = () => {
-  // const hello = api.example.hello.useQuery({ text: "from tRPC" });
-  const { data, error, isLoading } = useDrink();
-  // const handleClick = () => {
-  //   console.log("a drink was selected", hello);
-  // };
-  if (error) return <div>Request Failed</div>;
-  if (isLoading) return <div>Loading</div>;
-  console.log(data);
+  const { data, isLoading, isError } = useDrink();
+  const [randomDrinks, setRandomDrinks] = useState(getRandomDrinks());
+  if (isLoading) return <div>null</div>;
+  if (isError) return <div>Request Failed</div>;
+
+  const handleClick = (drinkName: string) => {
+    console.log(drinkName);
+    setRandomDrinks(getRandomDrinks());
+  };
   return (
     <>
       <Head>
@@ -28,25 +31,46 @@ const Home: NextPage = () => {
             Rate That <span className="text-pink-600">Drink</span>
           </h1>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-            <div
-              // onClick={handleClick}
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            >
-              <h3 className="text-2xl font-bold">Old fashioned</h3>
-              <div className="text-lg">Photo</div>
-            </div>
-            <div
-              // onClick={handleClick}
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            >
-              <h3 className="text-2xl font-bold">Daiquiri</h3>
-              <div className="text-lg">Photo</div>
-            </div>
+            {data.drinks && (
+              <>
+                <div
+                  onClick={() =>
+                    handleClick(data.drinks[randomDrinks.first]?.strDrink)
+                  }
+                  className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white cursor-pointer hover:bg-white/20"
+                >
+                  <h3 className="text-2xl font-bold text-center">
+                    {data.drinks[randomDrinks.first]?.strDrink}
+                  </h3>
+                  <Image
+                    src={data.drinks[randomDrinks.first]?.strDrinkThumb}
+                    alt="First drink thumb"
+                    width={350}
+                    height={350}
+                    className="animate-fade-in"
+                  />
+                </div>
+                <div
+                  onClick={() =>
+                    handleClick(data.drinks[randomDrinks.second]?.strDrink)
+                  }
+                  className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white cursor-pointer hover:bg-white/20"
+                >
+                  <h3 className="text-2xl font-bold text-center">
+                    {data.drinks[randomDrinks.second]?.strDrink}
+                  </h3>
+                  <Image
+                    src={data.drinks[randomDrinks.second]?.strDrinkThumb}
+                    alt="First drink thumb"
+                    width={350}
+                    height={350}
+                    className="animate-fade-in"
+                  />
+                </div>
+              </>
+            )}
           </div>
         </div>
-        {/* <p className="text-2xl text-white">
-          {hello.data ? hello.data.greeting : "Loading tRPC query..."}
-        </p> */}
       </main>
     </>
   );
